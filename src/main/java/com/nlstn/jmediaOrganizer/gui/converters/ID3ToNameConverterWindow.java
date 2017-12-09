45c0c52af084c6e69cc45077a4a8f6d2726a84a7
Niklas Lahnstein <niklas.lahnstein@gmail.com>
2017-12-09 15:50:38 +0100

    JMO-14 Saving converter settings

--- src/main/java/com/nlstn/jmediaOrganizer/gui/converters/ID3ToNameConverterWindow.java
+++ src/main/java/com/nlstn/jmediaOrganizer/gui/converters/ID3ToNameConverterWindow.java
@@ -1,17 +1,25 @@
 package com.nlstn.jmediaOrganizer.gui.converters;
 
+import java.awt.Color;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-import javax.swing.JButton;
+import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
-import com.nlstn.jmediaOrganizer.gui.Window;
+import com.nlstn.jmediaOrganizer.Settings;
 import com.nlstn.jmediaOrganizer.processing.MP3File;
 
 public class ID3ToNameConverterWindow extends JDialog {
@@ -24,6 +32,8 @@ public class ID3ToNameConverterWindow extends JDialog {
 
 	private JTextArea			lblExample;
 
+	private JCheckBox			chkEnabled;
+
 	private String				exampleArtist		= "Linkin Park";
 	private String				exampleAlbum		= "A Thousand Suns";
 	private String				exampleTrack		= "12";
@@ -34,18 +44,38 @@ public class ID3ToNameConverterWindow extends JDialog {
 		setSize(width, height);
 		setLocationRelativeTo(mainFrame);
 		setLayout(null);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				saveSettings();
+			}
+		});
+
+		chkEnabled = new JCheckBox();
+		chkEnabled.setBounds(10, 7, 15, 15);
+		chkEnabled.addActionListener((ActionEvent e) -> onToggleEnabled());
+		add(chkEnabled);
+
+		JLabel lblEnabled = new JLabel("Enabled");
+		lblEnabled.setBounds(30, 7, 50, 15);
+		add(lblEnabled);
 
 		lblExample = new JTextArea();
-		lblExample.setBounds(10, 10, 580, 130);
+		lblExample.setBounds(10, 27, 560, 130);
 		lblExample.setEditable(false);
+
+		Border lineBorder = BorderFactory.createLineBorder(Color.BLACK);
+		Border insets = new EmptyBorder(5, 5, 5, 5);
+
+		lblExample.setBorder(new CompoundBorder(lineBorder, insets));
+		lblExample.setMargin(new Insets(5, 5, 5, 5));
 		add(lblExample);
 
 		JLabel lblPattern = new JLabel("Pattern:");
-		lblPattern.setBounds(10, 150, 50, 28);
+		lblPattern.setBounds(10, 167, 50, 28);
 		add(lblPattern);
 
 		txtPattern = new JTextField();
-		txtPattern.setBounds(70, 150, 380, 28);
+		txtPattern.setBounds(70, 167, 460, 28);
 		txtPattern.getDocument().addDocumentListener(new DocumentListener() {
 
 			public void insertUpdate(DocumentEvent e) {
@@ -63,17 +93,31 @@ public class ID3ToNameConverterWindow extends JDialog {
 		});
 		add(txtPattern);
 
-		JButton button = new JButton("Convert all");
-		button.addActionListener((ActionEvent e) -> onConvert());
-		button.setBounds(width / 2 - Window.BUTTON_WIDTH / 2, height - 100, Window.BUTTON_WIDTH, 28);
-		add(button);
-
 		if (preview != null)
 			setPreviewData(preview.getTrack(), preview.getTitle(), preview.getArtist(), preview.getAlbum());
+
+		loadSettings();
 
 		lblExample.setText(buildPreview());
 
 		setVisible(true);
+	}
+
+	private void onToggleEnabled() {
+		txtPattern.setEnabled(chkEnabled.isSelected());
+		lblExample.setEnabled(chkEnabled.isSelected());
+	}
+
+	private void loadSettings() {
+		chkEnabled.setSelected(Settings.getID3ToNameEnabled());
+		txtPattern.setText(Settings.getID3ToNamePattern());
+		onToggleEnabled();
+	}
+
+	private void saveSettings() {
+		Settings.setID3ToNameEnabled(chkEnabled.isSelected());
+		Settings.setID3ToNamePattern(txtPattern.getText());
+		Settings.save();
 	}
 
 	public void setPreviewData(String track, String title, String artist, String album) {
@@ -96,10 +140,6 @@ public class ID3ToNameConverterWindow extends JDialog {
 			builder.append(pattern);
 		}
 		return builder.toString();
-	}
-
-	private void onConvert() {
-
 	}
 
 }
