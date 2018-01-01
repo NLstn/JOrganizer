b9f76dbf00c41f3346df274b7c7cce2bc654c00d
Niklas Lahnstein <niklas.lahnstein@gmail.com>
2018-01-01 19:24:46 +0100

    JMO-67 Replaced java properties with apache configurations

--- src/main/java/com/nlstn/jmediaOrganizer/properties/ConfigurationHandler.java
+++ src/main/java/com/nlstn/jmediaOrganizer/properties/ConfigurationHandler.java
@@ -1,14 +1,19 @@
 package com.nlstn.jmediaOrganizer.properties;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.XMLConfiguration;
 import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.builder.fluent.Parameters;
 import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -27,13 +32,16 @@ public class ConfigurationHandler {
 		configs = new Configurations();
 	}
 
-	private FileBasedConfigurationBuilder<FileBasedConfiguration>	builder;
-	private FileBasedConfiguration									config;
+	private FileBasedConfigurationBuilder<XMLConfiguration>	builder;
+	private FileBasedConfiguration							config;
 
 	public ConfigurationHandler(String fileName) {
 		File propertiesFile = new File(System.getProperty("jmediaOrganizer.home") + "\\" + fileName);
+		if (!propertiesFile.exists()) {
+			copySampleFile(fileName, propertiesFile);
+		}
 		Parameters params = new Parameters();
-		builder = configs.fileBasedBuilder(FileBasedConfiguration.class, propertiesFile).configure(params.fileBased().setListDelimiterHandler(new DefaultListDelimiterHandler(';')));
+		builder = configs.fileBasedBuilder(XMLConfiguration.class, propertiesFile).configure(params.fileBased().setThrowExceptionOnMissing(false).setFile(propertiesFile).setListDelimiterHandler(new DefaultListDelimiterHandler(';')));
 		try {
 			config = builder.getConfiguration();
 		}
@@ -52,6 +60,28 @@ public class ConfigurationHandler {
 		}
 		catch (ConfigurationException e) {
 			log.error("Failed to save configuration!", e);
+		}
+	}
+
+	private static void copySampleFile(String fileName, File propertiesFile) {
+		FileOutputStream stream = null;
+		InputStream exampleFile = null;
+		try {
+			stream = new FileOutputStream(propertiesFile);
+			exampleFile = ConfigurationHandler.class.getResourceAsStream("/" + fileName);
+			IOUtils.copy(exampleFile, stream);
+		}
+		catch (IOException e) {
+			log.error("Failed to copy sample settings file!", e);
+		}
+		finally {
+			try {
+				stream.close();
+				exampleFile.close();
+			}
+			catch (IOException e) {
+				log.error("Failed to copy sample settings file!", e);
+			}
 		}
 	}
 
